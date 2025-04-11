e4544950dd5b69d700460f5b783d0750d73d248d
beatrizgomez283 <beatrizgomez283@gmail.com>
2025-04-11 19:29:34 +0200

    asefeses

--- js/data.js
+++ js/data.js
@@ -1,42 +1,24 @@
-
-export const workouts = [
+const workouts = [
+  {
+    name: "Workout 1",
+    days: [
       {
-        name: "Workout 1",
-        days: [
-          {
-            name: "Day 1",
-            exercises: [
-              {
-                name: "Squat",
-                sets: 3,
-                target: 12,
-                type: "reps"
-              },
-              {
-                name: "Deadlift",
-                sets: 3,
-                target: 10,
-                type: "reps"
-              }
-            ]
-          },
-          {
-            name: "Day 2",
-            exercises: [
-              {
-                name: "Bench Press",
-                sets: 4,
-                target: 10,
-                type: "reps"
-              },
-              {
-                name: "Pull Up",
-                sets: 3,
-                target: 8,
-                type: "reps"
-              }
-            ]
-          }
+        name: "Day 1",
+        exercises: [
+          { name: "Push-ups", sets: 3, target: 15, type: "reps" },
+          { name: "Plank", sets: 3, target: 30, type: "secs" }
         ]
-      } 
+      },
+      {
+        name: "Day 2",
+        exercises: [
+          { name: "Squats", sets: 4, target: 12, type: "reps" },
+          { name: "Jumping Jacks", sets: 4, target: 40, type: "secs" }
+        ]
+      }
+    ]
+  }
 ];
+
+// Luego, puedes usar workouts
+console.log(workouts);
