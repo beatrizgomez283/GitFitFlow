5d17efb2d9bff9082f949d9bcd221bb35555196c
beatriz.gcarrero <beatriz.gcarrero@ESL-9W70V04>
2025-04-12 06:34:46 +0200

    asdas

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-
-// index.js
 import { workouts } from '../data/workouts.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const workoutListDiv = document.getElementById('workout-list');
 
-  // Iterar sobre los workouts y agregar los enlaces correspondientes a la lista
   workouts.forEach((workout, workoutIndex) => {
     const workoutItem = document.createElement('div');
-    workoutItem.className = 'workout-item';
+    workoutItem.className = 'card'; // Usa clase card para que sea un rectángulo blanco
     workoutItem.innerHTML = `<h3>${workout.name}</h3>`;
 
     const dayList = document.createElement('ul');
-    
-    // Iterar sobre los días de cada workout
+
     workout.days.forEach((day, dayIndex) => {
       const dayItem = document.createElement('li');
-      //dayItem.innerHTML = `<a href="day-list.html?workout=${workoutIndex}&day=${dayIndex}">${day.name}</a>`;
-      dayItem.innerHTML = `<a href="day-list.html?workoutIndex=${workoutIndex}&dayIndex={dayIndex}">${day.name}</a>`;
+      dayItem.innerHTML = `
+        <a class="day-link" href="day-list.html?workoutIndex=${workoutIndex}&dayIndex=${dayIndex}">
+          ${day.name}
+        </a>
+      `;
       dayList.appendChild(dayItem);
     });
 
