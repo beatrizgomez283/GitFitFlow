a0c0aec743d9a154acc1fd637c7a90929936f057
beatriz.gcarrero <beatriz.gcarrero@ESL-9W70V04>
2025-04-15 10:41:38 +0200

    asasd

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
+    base: './', // ✅ esto es clave
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
-    base: './',
-})
+});
